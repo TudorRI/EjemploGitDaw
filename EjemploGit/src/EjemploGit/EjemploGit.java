c731eca30a0438189f0507cb5c06a56cdeda410d
TudorRI <166540841+TudorRI@users.noreply.github.com>
2024-04-18 15:54:45 +0200

    Initial Commit

---
+++ EjemploGit/src/EjemploGit/EjemploGit.java
@@ -0,0 +1,11 @@
+package EjemploGit;
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola,Mundo");
+
+	}
+
+}
---
+++ Entornos/GitHub/Ejemplo1/Ejemplo.java
@@ -0,0 +1,11 @@
+package Ejemplo1;
+
+public class Ejemplo {
+	
+	public static void Main(String [] Args) {
+		
+		System.out.println("Hola Mundo");
+		
+	}
+	
+}
